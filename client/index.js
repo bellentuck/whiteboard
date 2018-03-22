34c1d3d977469190b3604693aa1953ea964965eb
Da Woon Lee <daniel.lee9415@gmail.com>
2018-03-22 18:02:05 -0400

    first commit

--- client/index.js
+++ client/index.js
@@ -1,7 +1,21 @@
 // Import from the module './whiteboard':
 //   The default export, naming it draw,
 //   An export named `events`, calling it `whiteboard`.
-import whiteboard, {draw} from './whiteboard'
+import whiteboard, { draw } from './whiteboard';
 
 // Example: Draw a single stroke.
-draw([0, 0], [250, 250], 'red', true)
+draw([0, 0], [250, 250], 'red', true);
+const socket = io(window.location.origin);
+
+whiteboard.on('draw', payload => {
+  socket.emit('draw', payload);
+});
+
+socket.on('connect', message => {
+  console.log(message);
+});
+
+socket.on('updateDrawing', (payload) => {
+  console.log(payload);
+  draw(payload, payload, 'red', false);
+});
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const socketio = require('socket.io');
 
 // app.listen() returns an http.Server object
 // http://expressjs.com/en/4x/api.html#app.listen
@@ -8,4 +9,24 @@ const server = app.listen(1337, function () {
     console.log(`Listening on http://localhost:${server.address().port}`);
 });
 
+let io = socketio(server);
+
+io.on('connection', function (socket) {
+    /* This function receives the newly connected socket.
+       This function will be called for EACH browser that connects to our server. */
+    console.log('A new client has connected!');
+    console.log(socket.id);
+    socket.on('disconnect', () => {
+        console.log('disconnected from client');
+    });
+    socket.on('draw', (payload) => {
+        socket.broadcast.emit('updateDrawing', payload);
+    });
+});
+
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
