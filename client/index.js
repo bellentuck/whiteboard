2322353e1e75e6ed02d23a95d373938d35d6d42e
Ben Ellentuck <ellentuckben@gmail.com>
2018-03-23 10:25:51 -0400

    fluid drawing working now

--- client/index.js
+++ client/index.js
@@ -2,20 +2,33 @@
 //   The default export, naming it draw,
 //   An export named `events`, calling it `whiteboard`.
 import whiteboard, { draw } from './whiteboard';
+import io from 'socket.io-client';
 
 // Example: Draw a single stroke.
-draw([0, 0], [250, 250], 'red', true);
+//draw([0, 0], [250, 250], 'red', true);
+
 const socket = io(window.location.origin);
 
-whiteboard.on('draw', payload => {
-  socket.emit('draw', payload);
+socket.on('connect', () => {
+  console.log('connected to server');
 });
 
-socket.on('connect', message => {
-  console.log(message);
+socket.on('load', strokes => {
+  strokes.forEach(stroke => {
+    const { start, end, color } = stroke;
+    draw(start, end, color, false);
+  });
 });
 
-socket.on('updateDrawing', (payload) => {
-  console.log(payload);
-  draw(payload, payload, 'red', false);
+// render published draws to screen
+socket.on('draw', (...payload) => {
+  // console.log(payload);
+  draw(...payload, false);
 });
+
+// let others know about a particular drawing
+whiteboard.on('draw', (...payload) => {
+  socket.emit('draw', ...payload);
+});
+
+
--- server.js
+++ server.js
@@ -11,19 +11,37 @@ const server = app.listen(1337, function () {
 
 let io = socketio(server);
 
+const inMemoryDrawHistory = [];
+
 io.on('connection', function (socket) {
     /* This function receives the newly connected socket.
        This function will be called for EACH browser that connects to our server. */
     console.log('A new client has connected!');
     console.log(socket.id);
+
+    if (inMemoryDrawHistory.length) {
+        console.log('loading', inMemoryDrawHistory);
+        socket.emit('load', inMemoryDrawHistory);
+    }
+
+    socket.on('draw', (...payload) => {
+        [ start, end, color ] = payload;
+        inMemoryDrawHistory.push({start, end, color});
+        //socket.broadcast.emit('updateDrawing', payload);
+        socket.broadcast.emit('draw', ...payload);
+    });
+
     socket.on('disconnect', () => {
         console.log('disconnected from client');
     });
-    socket.on('draw', (payload) => {
-        socket.broadcast.emit('updateDrawing', payload);
-    });
 });
 
+io.on('disconnect', () => {
+   console.log('game over for now');
+});
+
+// ensures no partial compilaiton
+//app.use(require('./webpack-middleware'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
